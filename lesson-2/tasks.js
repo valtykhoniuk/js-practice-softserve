64bfd68a21b35d401be20dbf082f471a55b6ea57
valtykhoniuk <206243967+valtykhoniuk@users.noreply.github.com>
2025-04-06 00:44:31 +0200

    tasks 5-7 added

--- lesson-2/tasks.js
+++ lesson-2/tasks.js
@@ -28,3 +28,66 @@ function findUnique(arr) {
   let uniqueElements = new Set(arr);
   return uniqueElements.size === arr.length;
 }
+
+// task 5
+function getNumbers(a, b) {
+  let resultArr = [];
+
+  let counter = 1;
+  while (a <= b) {
+    for (let i = 1; i <= counter; i++) {
+      resultArr.push(a);
+    }
+    a++;
+    counter++;
+  }
+  console.log(resultArr);
+}
+
+// task 6
+function compact(arr) {
+  let uniqueSet = new Set(arr);
+  let uniqueArray = Array.from(uniqueSet);
+  return uniqueArray;
+}
+
+// task 7
+function funcName(arr) {
+  let numbers = [];
+  let strings = [];
+
+  arr.flat(Infinity);
+
+  arr.forEach((element) => {
+    if (typeof element === "number") {
+      numbers.push(element);
+    } else if (typeof element === "string") {
+      strings.push(element);
+    }
+  });
+
+  return [numbers, strings];
+}
+
+// advanced task
+function isPrime(n) {
+  if (n < 2) return false;
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
+}
+
+function generatePrimes(count) {
+  let primes = [];
+  let num = 2;
+
+  while (primes.length < count) {
+    if (isPrime(num)) {
+      primes.push(num);
+    }
+    num++;
+  }
+
+  return primes;
+}
