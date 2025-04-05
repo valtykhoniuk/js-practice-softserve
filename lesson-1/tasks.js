4ae030a21c4197c352d865b351552970bc6836fd
valtykhoniuk <206243967+valtykhoniuk@users.noreply.github.com>
2025-04-05 12:22:53 +0200

    lesson 1 tasks added

---
+++ lesson-1/tasks.js
@@ -0,0 +1,16 @@
+// task 1
+let age = Number(prompt("How old are you?"));
+if (age >= 18) {
+  console.log("Fine");
+} else {
+  console.log("Sorry, no");
+}
+
+// task 2
+let email = String(prompt("Enter your email"));
+let name = String(prompt("Enter your name"));
+
+console.log("Hi, " + name + " your email is " + email);
+
+// task  3
+let isAdult = confirm("Are you adult?");
