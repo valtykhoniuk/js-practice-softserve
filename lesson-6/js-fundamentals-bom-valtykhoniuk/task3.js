b92c8f8db9801088efe452541733f45da15bc67a
valtykhoniuk <206243967+valtykhoniuk@users.noreply.github.com>
2025-04-07 23:10:45 +0200

    lesson 6 task finished

--- lesson-6/js-fundamentals-bom-valtykhoniuk/task3.js
+++ lesson-6/js-fundamentals-bom-valtykhoniuk/task3.js
@@ -1,5 +1,14 @@
-// Реалізуйте програму, яка по натисканню на кнопку видалятиме обраний елемент випадаючого списку. 
+// Реалізуйте програму, яка по натисканню на кнопку видалятиме обраний елемент випадаючого списку.
 // Можуть видалятися всі елементи в будь-якому порядку.
 // Список з елементами знаходиться у файлі index.html
 // Для запуску використовувати Live Server
 // Для перевірки виконання виконати команду node tests/task3.test.js
+
+document.getElementById("deleteButton").addEventListener("click", function () {
+  const dropdown = document.getElementById("dropdown");
+  const selectedIndex = dropdown.selectedIndex;
+
+  if (selectedIndex !== -1) {
+    dropdown.remove(selectedIndex);
+  }
+});
--- lesson-6/js-fundamentals-bom-valtykhoniuk/task4.js
+++ lesson-6/js-fundamentals-bom-valtykhoniuk/task4.js
@@ -2,3 +2,15 @@
 //  і виводитиме на поточну сторінку при її розтязі/стисканні відповідні значення.
 // Для запуску використовувати Live Server
 // Для перевірки node tests/task4.test.js
+
+function updateWindowSize() {
+  const widthSpan = document.getElementById("widthValue");
+  const heightSpan = document.getElementById("heightValue");
+
+  widthSpan.textContent = window.innerWidth;
+  heightSpan.textContent = window.innerHeight;
+}
+
+updateWindowSize();
+
+window.addEventListener("resize", updateWindowSize);
